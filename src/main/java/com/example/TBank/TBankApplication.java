861601aff7135287d197fe7a5827294dc7c854d9
tessaag <tessageorge.2003@gmail.com>
2025-02-27 16:39:45 +0530

    Main

---
+++ src/main/java/com/example/TBank/TBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.TBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TBank/TBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
